f90ce600ff017fac8839408aa1168d8f820e5497
John Sauter <John_Sauter@systemeyescomputerstore.com>
2022-08-16 13:44:04 -0400

    Saved books also link to the Google preview

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -109,6 +109,11 @@ const SavedBooks = () => {
                 <Card.Body>
                   <Card.Title>{book.title}</Card.Title>
                   <p className="small">Authors: {book.authors}</p>
+                  <p>
+                    <a href={book.previewLink} rel="noreferrer" target="_blank">
+                      Google preview
+                    </a>
+                  </p>
                   <Card.Text>{book.description}</Card.Text>
                   <Button
                     className="btn-block btn-danger"
